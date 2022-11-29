78923f15e19307a8f54e2bd62ba3cd8184f81ffd
Gerf <73294402+GerfautGE@users.noreply.github.com>
2022-11-29 12:04:02 +0100

    feat(whoami): add whoami
    - whoami title
    - console component
    - console response are in console.json

--- src/Components/Whoami/Whoami.tsx
+++ src/Components/Whoami/Whoami.tsx
@@ -1,9 +1,11 @@
 import './Whoami.scss';
+import Console from './Console/Console';
 
 function Whoami(){
     return (
         <div className="whoami">
             <h2>Whoami</h2>
+            <Console />
         </div>
     )
 }
