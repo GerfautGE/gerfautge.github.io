6c09fc041e1d2e1423187a6a192d37465dc32eb0
Gerf <73294402+GerfautGE@users.noreply.github.com>
2022-11-29 14:03:07 +0100

    feat(whoami): add image next to console

--- src/Components/Whoami/Whoami.tsx
+++ src/Components/Whoami/Whoami.tsx
@@ -1,11 +1,16 @@
 import './Whoami.scss';
 import Console from './Console/Console';
 
-function Whoami(){
+import gerf from '../../static/gerf.png';
+
+function Whoami() {
     return (
         <div className="whoami">
             <h2>Whoami</h2>
-            <Console />
+            <div className="whoami__content">
+                <Console/>
+                <img src={gerf} alt="gerf.png"/>
+            </div>
         </div>
     )
 }
