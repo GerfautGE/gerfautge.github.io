f0278ab2e5a77c24991741548e8bae8974a7d020
Gerf <73294402+GerfautGE@users.noreply.github.com>
2022-11-29 10:13:31 +0100

    feat(Whoami) create file structure for Whoami

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./Components/Header/Header";
+import Whoami from "./Components/Whoami/Whoami";
 
 function App() {
   return (
     <div>
       <Header />
+      <Whoami />
     </div>
   );
 }
---
+++ src/Components/Whoami/Whoami.tsx
@@ -0,0 +1,11 @@
+import './Whoami.scss';
+
+function Whoami(){
+    return (
+        <div className="whoami">
+            <h2>Whoami</h2>
+        </div>
+    )
+}
+
+export default Whoami
